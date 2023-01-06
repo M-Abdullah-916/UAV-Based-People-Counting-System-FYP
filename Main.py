d88594ec46ac0ef1fb001f4a2b74882960fb6a0d
M Abdullah <f190916@nu.edu.pk>
2023-01-06 21:41:51 +0500

    Main.py intiallized

---
+++ Main.py
@@ -0,0 +1,13 @@
+import string
+import tkinter
+import time
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+
+if __name__ == '__main__':
+    print('Main Created')
+
