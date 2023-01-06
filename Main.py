6e5e55d53d869ca1d15e5d70e234b3122e3497d8
M Abdullah <f190916@nu.edu.pk>
2023-01-06 21:43:59 +0500

    mainWindow() function created and implemented in Main.py

--- Main.py
+++ Main.py
@@ -35,6 +35,48 @@ def progress():
         time.sleep(1)
 
 
+def mainWindow():
+    splashWindow.destroy()
+    main = Tk()
+    main.title('UAV-BASED PEOPLE COUNTING SYSTEM')
+    main.iconbitmap('Logo.ico')
+    main.minsize(1000, 700)
+    main.maxsize(1000, 700)
+    main.geometry('1000x700')
+
+    main.configure(background='#0FB5DA')
+
+    logo = Image.open('Logo.PNG')
+    resized_logo = logo.resize((290, 130))
+    logo = ImageTk.PhotoImage(resized_logo)
+    logo_label = Label(main, image=logo, background='#0FB5DA')
+    logo_label.pack(pady=(10, 10))
+
+    text_logo = Label(main, text='UAV-Based People Counting System', fg='white', bg='#0FB5DA')
+    text_logo.config(font=('verdana', 24))
+    text_logo.pack()
+
+    add_image_button = Button(main, text='Add Images', command=addImage, bg='white', fg='black', width=30, height=2)
+    add_image_button.pack(pady=(15, 5))
+
+    manage_drone_button = Button(main, text='Provide Coordinates to Drone', command=manageDrone, bg='white', fg='black', width=30, height=2)
+    manage_drone_button.pack(pady=(5, 5))
+
+    collate_image_button = Button(main, text='Collate Images', command=imageCollation, bg='white', fg='black', width=30,
+                                  height=2)
+    collate_image_button.pack(pady=(5, 5))
+
+    history_button = Button(main, text='Check History', command=checkHistory, bg='white', fg='black', width=30,
+                            height=2)
+    history_button.pack(pady=(5, 5))
+
+    exit_button = Button(main, text='Exit', command=main.destroy, bg='white', fg='black', width=30, height=2)
+    exit_button.pack(pady=(5, 5))
+
+    main.mainloop()
+
 if __name__ == '__main__':
-    print('Main Created')
+    progress()
+    splashWindow.after(3500, mainWindow)
+    mainloop()
 
