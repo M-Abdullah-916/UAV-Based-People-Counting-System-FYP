3974d7523786d336211b0bbb7a6e798cc8ad497d
M Abdullah <f190916@nu.edu.pk>
2023-01-09 01:13:28 +0500

    Image_Enhancement Class initiallized

---
+++ Image_Enhancement.py
@@ -0,0 +1,9 @@
+import numpy as np
+import Utils
+import Extract_Keypoints
+from matplotlib import pyplot as plt
+import cv2
+
+class Enhancement:
+    def __init__(self):
+        print("s")
--- Image_Stitching.py
+++ Image_Stitching.py
@@ -1,6 +1,18 @@
+import Image_Enhancement
 import Utils
-import matplotlib.pyplot as plt
 import Extract_Keypoints
+from matplotlib import pyplot as plt
+import cv2
+
+
+def adapt_image_to_color_scale(image, color_scale):
+    color = None
+    if (color_scale != 'gray'):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    else:
+        color = "gray"
+    return image, color
+
 
 
 class ImageCollation:
@@ -16,3 +28,18 @@ class ImageCollation:
         plt.title('Image After Collation')
         plt.imshow(Extract_Keypoints.extract_keypoints(image_list))
         plt.show()
+
+    def image_enhancement(self):
+        image_loading_object = Utils.ReceiveImages(self.imagesPath)
+        image_list = image_loading_object.load_images()
+        plt.figure(figsize=(10, 10))
+        plt.title('Enhancement (Local Thresholding) After Collation')
+        final_image = Extract_Keypoints.extract_keypoints(image_list)
+        output = Enhancement.local_thresholding(final_image, 7)
+        plt.imshow(output)
+        plt.show()
+        color_scale = "rgb"
+        show_couple_of_images(final_image, output, "Original", "Local Thresholding", color_scale)
+        show_couple_of_images(final_image, Enhancement.make_image_negative(final_image), "Original", "Negative", "rgb")
+
+
